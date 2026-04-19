3bbb74422ecf460b8496fbce95150c85b444485f
Jack Bentley <jlb10333@gmail.com>
2026-04-19 12:06:37 -0700

    Add tilemap png generation util

---
+++ assets/maps/tilesets/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tiles" tilewidth="8" tileheight="8" tilecount="128" columns="8">
+ <image source="tiles.png" width="64" height="128"/>
+</tileset>
